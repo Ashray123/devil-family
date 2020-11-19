f4db926e53b864535f4ebebcdb01bef59a2a50a7
ASHRAY <jha.ashray12@gmail.com>
2020-11-19 23:26:04 +0530

    modification in handleChange

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ constructor(){
     monsters: [],
     searchField: ''
 };
+
 }
 componentDidMount(){
   fetch('https://jsonplaceholder.typicode.com/users')
@@ -19,22 +20,24 @@ componentDidMount(){
   .then (users =>this.setState({monsters: users}));
 
 }
+handleChange= e => {
+  this.setState({searchField:e.target.value});
+} ;
 
   render() {
+    
  const {monsters , searchField} =this.state;
  const filteredMonsters = monsters.filter(monster => 
   monster.name.toLowerCase().includes(searchField.toLowerCase()));
 
-
-
   return (
     <div className="App">
 
-
   <SearchBox
+  
   placeholder='Search Devil'
-  handleChange={e => 
-  this.setState({searchField:e.target.value})}
+  
+  handleChange={this.handleChange}
   />
     <Cardlist monsters= {filteredMonsters} />
     </div>
